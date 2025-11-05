d3ca923f4fed348baf9a7463161e16bc08eb0213
Mordy <theojohnhenry@gmail.com>
2025-11-05 20:00:31 +0100

    fuck

--- init.lua
+++ init.lua
@@ -945,14 +945,14 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.toggleterm',
 
-  require 'custom.themes.gruvbox',
+  -- require 'custom.themes.gruvbox',
   -- require 'custom.themes.ayu',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'miikanissi/modus-themes.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'modus_vivendi'
+    end,
+  },
+}
